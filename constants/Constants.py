e41803c5c49f6b500274344c9442e9fb05c33073
vaa1000 <69126450+vaa1000@users.noreply.github.com>
2022-11-08 23:13:38 +0300

    wrap code in class Other

--- constants/Constants.py
+++ constants/Constants.py
@@ -1,15 +1,17 @@
-BASE_URL_PLANETS = 'https://swapi.dev/api/planets/'
-BASE_URL_PEOPLES = 'https://swapi.dev/api/people/'
+class Other:
+    
+    BASE_URL_PLANETS = 'https://swapi.dev/api/planets/'
+    BASE_URL_PEOPLES = 'https://swapi.dev/api/people/'
 
-PLANETS = [
-    ('1', 'Tatooine'),
-    ('2', 'Alderaan'),
-    ('3', 'Yavin IV'),
-    ('4', 'Hoth'),
-    ('10', 'Kamino'),
-    #('15', 'Polis Massa')
+    PLANETS = [
+        ('1', 'Tatooine'),
+        ('2', 'Alderaan'),
+        ('3', 'Yavin IV'),
+        ('4', 'Hoth'),
+        ('10', 'Kamino'),
+        #('15', 'Polis Massa')
 
-]
+    ]
 
-DIAMETER_EARTH = 12743
+    DIAMETER_EARTH = 12743
 
